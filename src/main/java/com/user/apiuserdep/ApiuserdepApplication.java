06ce4a019065994c69c9dcc71124a31ac2d5be6d
lucenasoft <getugbr@gmail.com>
2023-01-20 23:32:22 -0300

    feat: start project in spring initializr

---
+++ src/main/java/com/user/apiuserdep/ApiuserdepApplication.java
@@ -0,0 +1,13 @@
+package com.user.apiuserdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiuserdepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiuserdepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/apiuserdep/ApiuserdepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.apiuserdep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiuserdepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
